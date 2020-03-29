b3c16a76f4b239aafca2bad938a2dd8bd7483c28
catarinamachado <catarinamachado11@gmail.com>
2020-03-29 18:51:01 +0100

    Add today date (footer)

--- js/script.js
+++ js/script.js
@@ -1,34 +1,46 @@
 //Grafico lines
 var ctxL = document.getElementById("lineChart").getContext('2d');
 var myLineChart = new Chart(ctxL, {
-type: 'line',
-data: {
-labels: ["January", "February", "March", "April", "May", "June", "July"],
-datasets: [{
-label: "My First dataset",
-data: [65, 59, 80, 81, 56, 55, 40],
-backgroundColor: [
-'rgba(105, 0, 132, .2)',
-],
-borderColor: [
-'rgba(200, 99, 132, .7)',
-],
-borderWidth: 2
-},
-{
-label: "My Second dataset",
-data: [28, 48, 40, 19, 86, 27, 90],
-backgroundColor: [
-'rgba(0, 137, 132, .2)',
-],
-borderColor: [
-'rgba(0, 10, 130, .7)',
-],
-borderWidth: 2
-}
-]
-},
-options: {
-responsive: true
-}
+    type: 'line',
+    data: {
+        labels: ["January", "February", "March", "April", "May", "June", "July"],
+        datasets: [{
+            label: "My First dataset",
+            data: [65, 59, 80, 81, 56, 55, 40],
+            backgroundColor: [
+                'rgba(105, 0, 132, .2)',
+            ],
+            borderColor: [
+            'rgba(200, 99, 132, .7)',
+            ],
+            borderWidth: 2
+        },
+        {
+            label: "My Second dataset",
+            data: [28, 48, 40, 19, 86, 27, 90],
+            backgroundColor: [
+            'rgba(0, 137, 132, .2)',
+            ],
+            borderColor: [
+            'rgba(0, 10, 130, .7)',
+            ],
+            borderWidth: 2
+        }
+    ]
+    },
+    options: {
+        responsive: true
+    }
 });
+
+// Data atual
+var d = new Date();
+
+var month = d.getMonth()+1;
+var day = d.getDate();
+
+var output = (day<10 ? '0' : '') + day + '/' +
+             (month<10 ? '0' : '') + month + '/' +
+             d.getFullYear();
+
+$("#data").html(output);
