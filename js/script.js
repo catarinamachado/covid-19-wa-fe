0754ed88cfa5430cc54f889c23c29d3f7e78fcf7
catarinamachado <catarinamachado11@gmail.com>
2020-04-03 22:31:36 +0100

    Add finished Table of Countries

--- js/script.js
+++ js/script.js
@@ -56,8 +56,9 @@ $.getJSON('http://localhost:8000/overallData', function(data) {
 //Tabela total países
 $(document).ready(function () {
     $('#dtTable').DataTable({
-        "searching": false,
+        dom:"<'myfilter'f>tpi",
         "lengthChange": false,
+        pageLength: 8,
         "aaSorting": [],
         columnDefs: [{
             orderable: false,
@@ -73,9 +74,9 @@ $(document).ready(function () {
             "sLengthMenu":    "Mostrar _MENU_ registos",
             "sZeroRecords":   "Sem resultados",
             "sEmptyTable":    "Sem dados disponível",
-            "sInfo":          "Mostrando _START_ até _END_ em _TOTAL_",
+            "sInfo":          "Mostrando _START_ até _END_ num total de _TOTAL_",
             "sInfoEmpty":     "Mostrando 0 a 0 em 0",
-            "sInfoFiltered":  "(filtrado de um total de _MAX_ registos)",
+            "sInfoFiltered":  "(filtrado num total de _MAX_ registos)",
             "sInfoPostFix":   "",
             "sSearch":        "Procurar:",
             "sUrl":           "",
@@ -100,28 +101,221 @@ $("#checkAll").click(function () {
     $(".check").prop('checked', $(this).prop('checked'));
 });
 
-
 var countries = {
-    'PT':'Portugal', 
-    'ES':'Espanha'
-};
+    "AF": "Afghanistan",
+    "AL": "Albania",
+    "DZ": "Algeria",
+    "AO": "Angola",
+    "AR": "Argentina",
+    "AM": "Armenia",
+    "AU": "Australia",
+    "AT": "Austria",
+    "AZ": "Azerbaijan",
+    "BS": "Bahamas",
+    "BD": "Bangladesh",
+    "BY": "Belarus",
+    "BE": "Belgium",
+    "BZ": "Belize",
+    "BJ": "Benin",
+    "BT": "Bhutan",
+    "BO": "Bolivia",
+    "BA": "Bosnia and Herzegovina",
+    "BW": "Botswana",
+    "BR": "Brazil",
+    "BN": "Brunei Darussalam",
+    "BG": "Bulgaria",
+    "BF": "Burkina Faso",
+    "BI": "Burundi",
+    "KH": "Cambodia",
+    "CM": "Cameroon",
+    "CA": "Canada",
+    "CI": "Ivory Coast",
+    "CF": "Central African Republic",
+    "TD": "Chad",
+    "CL": "Chile",
+    "CN": "China",
+    "CO": "Colombia",
+    "CG": "Congo",
+    "CD": "Democratic Republic of Congo",
+    "CR": "Costa Rica",
+    "HR": "Croatia",
+    "CU": "Cuba",
+    "CY": "Cyprus",
+    "CZ": "Czechia",
+    "DK": "Denmark",
+    "DP": "Diamond Princess",
+    "DJ": "Djibouti",
+    "DO": "Dominican Republic",
+    "CD": "DR Congo",
+    "EC": "Ecuador",
+    "EG": "Egypt",
+    "SV": "El Salvador",
+    "GQ": "Equatorial Guinea",
+    "ER": "Eritrea",
+    "EE": "Estonia",
+    "ET": "Ethiopia",
+    "FK": "Falkland Islands",
+    "FJ": "Fiji",
+    "FI": "Finland",
+    "FR": "France",
+    "GF": "French Guiana",
+    "TF": "French Southern Territories",
+    "GA": "Gabon",
+    "GM": "Gambia",
+    "GE": "Georgia",
+    "DE": "Germany",
+    "GH": "Ghana",
+    "GR": "Greece",
+    "GL": "Greenland",
+    "GT": "Guatemala",
+    "GN": "Guinea",
+    "GW": "Guinea-Bissau",
+    "GY": "Guyana",
+    "HT": "Haiti",
+    "HN": "Honduras",
+    "HK": "Hong Kong",
+    "HU": "Hungary",
+    "IS": "Iceland",
+    "IN": "India",
+    "ID": "Indonesia",
+    "IR": "Iran",
+    "IQ": "Iraq",
+    "IE": "Ireland",
+    "IL": "Israel",
+    "IT": "Italy",
+    "JM": "Jamaica",
+    "JP": "Japan",
+    "JO": "Jordan",
+    "KZ": "Kazakhstan",
+    "KE": "Kenya",
+    "KP": "Korea",
+    "XK": "Kosovo",
+    "KW": "Kuwait",
+    "KG": "Kyrgyzstan",
+    "LA": "Lao",
+    "LV": "Latvia",
+    "LB": "Lebanon",
+    "LS": "Lesotho",
+    "LR": "Liberia",
+    "LY": "Libya",
+    "LT": "Lithuania",
+    "LU": "Luxembourg",
+    "MK": "Macedonia",
+    "MG": "Madagascar",
+    "MW": "Malawi",
+    "MY": "Malaysia",
+    "ML": "Mali",
+    "MR": "Mauritania",
+    "MX": "Mexico",
+    "MD": "Moldova",
+    "MN": "Mongolia",
+    "ME": "Montenegro",
+    "MA": "Morocco",
+    "MZ": "Mozambique",
+    "MM": "Myanmar",
+    "NA": "Namibia",
+    "NP": "Nepal",
+    "NL": "Netherlands",
+    "NC": "New Caledonia",
+    "NZ": "New Zealand",
+    "NI": "Nicaragua",
+    "NE": "Niger",
+    "NG": "Nigeria",
+    "KP": "North Korea",
+    "NO": "Norway",
+    "OM": "Oman",
+    "PK": "Pakistan",
+    "PS": "Palestine",
+    "PA": "Panama",
+    "PG": "Papua New Guinea",
+    "PY": "Paraguay",
+    "PE": "Peru",
+    "PH": "Philippines",
+    "PL": "Poland",
+    "PT": "Portugal",
+    "PR": "Puerto Rico",
+    "QA": "Qatar",
+    "XK": "Republic of Kosovo",
+    "RO": "Romania",
+    "RU": "Russia",
+    "RW": "Rwanda",
+    "SA": "Saudi Arabia",
+    "SN": "Senegal",
+    "RS": "Serbia",
+    "SL": "Sierra Leone",
+    "SG": "Singapore",
+    "SK": "Slovakia",
+    "SI": "Slovenia",
+    "SB": "Solomon Islands",
+    "SO": "Somalia",
+    "ZA": "South Africa",
+    "KR": "South Korea",
+    "SS": "South Sudan",
+    "ES": "Spain",
+    "LK": "Sri Lanka",
+    "SD": "Sudan",
+    "SR": "Suriname",
+    "SJ": "Svalbard and Jan Mayen",
+    "SZ": "Swaziland",
+    "SE": "Sweden",
+    "CH": "Switzerland",
+    "SY": "Syrian Arab Republic",
+    "TW": "Taiwan",
+    "TJ": "Tajikistan",
+    "TZ": "Tanzania",
+    "TH": "Thailand",
+    "TL": "Timor-Leste",
+    "TG": "Togo",
+    "TT": "Trinidad and Tobago",
+    "TN": "Tunisia",
+    "TR": "Turkey",
+    "TM": "Turkmenistan",
+    "AE": "UAE",
+    "UG": "Uganda",
+    "GB": "United Kingdom",
+    "UA": "Ukraine",
+    "US": "USA",
+    "UY": "Uruguay",
+    "UZ": "Uzbekistan",
+    "VU": "Vanuatu",
+    "VE": "Venezuela",
+    "VN": "Vietnam",
+    "EH": "Western Sahara",
+    "YE": "Yemen",
+    "ZM": "Zambia",
+    "ZW": "Zimbabwe"
+}
 
 // Preencher tabela total países
-var count = Object.keys(countries).length;
+var countryKey;
 var tbody = '';
 var rowHTML = '';
+var totalNumber;
 
-for (var i = 0; i < count; i++) {
-    rowHTML += '<td><div class="checkbox"><label><input type="checkbox" class="check"></label></div></td>';
-    rowHTML += '<td class="align-middle">' + countries[i] + '</td>';
-    rowHTML += '<td class="align-middle" id="totalPT"></td>';
+function asignVariable(data){
+    $totalNumber = data;
+}
 
-    $.getJSON('http://localhost:8000/countryData?Country=PT', function(data) {
-        $("#totalPT").html(data.total);
+for (countryKey in countries) {
+    var countryName = countries[countryKey];
+    var apiAccess = 'http://localhost:8000/countryData?Country=' + countryKey;
+
+    $.ajax({
+        url: apiAccess,
+        async: false,
+        dataType: 'json',
+        success: function(data) {
+            totalNumber = data.total;
+        }
     });
+
+    rowHTML += '<td><div class="checkbox"><label><input type="checkbox" class="check"></label></div></td>';
+    rowHTML += '<td class="align-middle">' + countryName + '</td>';
+    rowHTML += '<td class="align-middle">' + totalNumber + '</td>';
 
     tbody += '<tr class="m-0">' + rowHTML + '</tr>';
     rowHTML = ''
 }
 
 document.getElementById('table-data').innerHTML = tbody;
+
