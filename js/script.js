1b354697bf271277f8b2d8269176657ba0c712c0
catarinamachado <catarinamachado11@gmail.com>
2020-04-03 21:04:14 +0100

    Preparing table to read data from api

--- js/script.js
+++ js/script.js
@@ -67,11 +67,12 @@ $(document).ready(function () {
         style: 'os',
         selector: 'td:first-child'
         },
+        "order": [[ 2, "desc" ]],
         language: {
             "sProcessing":    "Processando...",
             "sLengthMenu":    "Mostrar _MENU_ registos",
             "sZeroRecords":   "Sem resultados",
-            "sEmptyTable":    "Nenhum dados disponível",
+            "sEmptyTable":    "Sem dados disponível",
             "sInfo":          "Mostrando _START_ até _END_ em _TOTAL_",
             "sInfoEmpty":     "Mostrando 0 a 0 em 0",
             "sInfoFiltered":  "(filtrado de um total de _MAX_ registos)",
@@ -95,8 +96,32 @@ $(document).ready(function () {
     $('.dataTables_length').addClass('bs-select');
 });
 
-
-
 $("#checkAll").click(function () {
     $(".check").prop('checked', $(this).prop('checked'));
 });
+
+
+var countries = {
+    'PT':'Portugal', 
+    'ES':'Espanha'
+};
+
+// Preencher tabela total países
+var count = Object.keys(countries).length;
+var tbody = '';
+var rowHTML = '';
+
+for (var i = 0; i < count; i++) {
+    rowHTML += '<td><div class="checkbox"><label><input type="checkbox" class="check"></label></div></td>';
+    rowHTML += '<td class="align-middle">' + countries[i] + '</td>';
+    rowHTML += '<td class="align-middle" id="totalPT"></td>';
+
+    $.getJSON('http://localhost:8000/countryData?Country=PT', function(data) {
+        $("#totalPT").html(data.total);
+    });
+
+    tbody += '<tr class="m-0">' + rowHTML + '</tr>';
+    rowHTML = ''
+}
+
+document.getElementById('table-data').innerHTML = tbody;
