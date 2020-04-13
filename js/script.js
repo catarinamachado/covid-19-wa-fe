18d29cc9e3e82091ed1f3522b23206290455a5d6
catarinamachado <catarinamachado11@gmail.com>
2020-04-13 20:30:46 +0100

    Begin parse data csv

--- js/script.js
+++ js/script.js
@@ -35,9 +35,47 @@ function graphicLines(graphicType, nrDays, countriesNameList, countriesKeyList){
     return(result);
 }
 
+function chatisse() {
+    var apiAccess = 'localhost:5000/predictions?days=' + nrDeDiasSeleccionado + '&field=' + tipoDeGraficoSelecionado + '&country=PT,IT'
+    
+}
+
 nrDeDiasTotal = 30;
 nrDeDiasSeleccionado = 7;
 tipoDeGraficoSelecionado = "Total_Cases";
+
+//supostamente vai buscar e dá parse do csv
+$(document).ready(function() {
+    var apiAccess = 'http://localhost:5000/predictions?days=' + nrDeDiasSeleccionado + '&field=' + tipoDeGraficoSelecionado + '&country=PT'
+    $.ajax({
+        type: "GET",
+        url: apiAccess,
+        dataType: "text",
+        success: function(data) {
+            processData(data);
+        }
+     });
+});
+
+function processData(allText) {
+    var record_num = 4;  // or however many elements there are in each row
+    var allTextLines = allText.split(/\r\n|\n/);
+    var entries = allTextLines[0].split(',');
+    var lines = [];
+
+    var headings = entries.splice(0,record_num);
+    while (entries.length>0) {
+        var tarr = [];
+        for (var j=0; j<record_num; j++) {
+            tarr.push(headings[j]+":"+entries.shift());
+        }
+        lines.push(tarr);
+    }
+
+    console.log(lines);
+    alert(lines);
+}
+//fim
 
 function initialGraphic(){
     $.get("https://ipinfo.io", function(response) {
